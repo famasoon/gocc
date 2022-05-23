69b9a11065701a34115a4be88b5baf07fc42941e
FAMASoon <famasoon@gmail.com>
2022-05-23 23:23:43 +0900

    Add analyzer

---
+++ analyzer.go
@@ -0,0 +1,35 @@
+package gocc
+
+import (
+	"go/ast"
+
+	"github.com/famasoon/gocc/complexity"
+	"golang.org/x/tools/go/analysis"
+)
+
+var (
+	threshold int
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name:     "gocc",
+	Doc:      "checks cyclomatic complexity",
+	Run:      run,
+	Requires: []*analysis.Analyzer{complexity.Analyzer},
+}
+
+func init() {
+	Analyzer.Flags.IntVar(&threshold, "over", 10, "minimum complexity to report")
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	complexities := pass.ResultOf[complexity.Analyzer].(map[ast.Node]int)
+	for n, count := range complexities {
+		if count >= threshold {
+			fd := n.(*ast.FuncDecl)
+			name := getFuncName(fd)
+			pass.Reportf(n.Pos(), "func %s complexity=%d", name, count)
+		}
+	}
+	return nil, nil
+}
