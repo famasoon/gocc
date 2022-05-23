4b0e36d8bd66edea2995f2f8971c60b8e0dc90fa
FAMASoon <famasoon@gmail.com>
2022-05-23 22:37:55 +0900

    Change Count

--- complexity/complexity.go
+++ complexity/complexity.go
@@ -2,16 +2,41 @@ package complexity
 
 import (
 	"go/ast"
+	"go/token"
 )
 
 func Count(node ast.Node) int {
 	count := 1
 	ast.Inspect(node, func(node ast.Node) bool {
-		switch node.(type) {
+		switch n := node.(type) {
 		case *ast.IfStmt:
+			count += countCondition(n.Cond)
+		case *ast.ForStmt, *ast.RangeStmt:
 			count++
-		case *ast.ForStmt:
+		case *ast.CaseClause:
+			if n.List == nil {
+				count++
+				break
+			}
+			for _, stmt := range n.List {
+				count += countCondition(stmt)
+			}
+		case *ast.CommClause:
 			count++
+		}
+		return true
+	})
+	return count
+}
+
+func countCondition(condNode ast.Node) int {
+	count := 1
+	ast.Inspect(condNode, func(n ast.Node) bool {
+		switch n := n.(type) {
+		case *ast.BinaryExpr:
+			if n.Op == token.LAND || n.Op == token.LOR {
+				count++
+			}
 		}
 		return true
 	})
